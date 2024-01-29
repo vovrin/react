0a27abe5c76375bae4dfd7fef2a2df8201bc3d8d
Михайло Воврин <mishajlovovrinv@gmail.com>
2024-01-29 21:20:46 +0200

    react problem in styles

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Main from "./assets/mains/Mains"
+import Heads from './assets/articles/head/Heads'
+import { useState } from 'react'
+
+
+
+function App() {
+  return (
+    <div>
+      <Main/>
+      
+    </div>
+  )
+}
+
+export default App
